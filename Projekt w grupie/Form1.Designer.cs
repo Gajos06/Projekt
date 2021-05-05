ff20a78cd375180cb9fb5016d528ad824b3f9659
Damian Sygitowicz <UL0249095@edu.uni.lodz.pl>
2021-05-05 19:40:28 +0200

    again

--- Projekt w grupie/Form1.Designer.cs
+++ Projekt w grupie/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.autorzy = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -91,12 +93,32 @@
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(749, 447);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(247, 69);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(867, 52);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(303, 57);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1527, 608);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.autorzy);
@@ -115,6 +137,8 @@
         private System.Windows.Forms.Button autorzy;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
