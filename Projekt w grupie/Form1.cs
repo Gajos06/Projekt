d8c3077a2e4a738936cff9b19b064cfb6f98b4e2
BartoszCzerwinski123 <UL0238469@edu.uni.lodz.pl>
2021-05-05 19:49:57 +0200

    zabijcie mnie

--- Projekt w grupie/Form1.cs
+++ Projekt w grupie/Form1.cs
@@ -34,7 +34,13 @@ namespace Projekt_w_grupie
         {
             //Kutasiarzu
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
         //KOMENTARZ
         //KOMETARZ NUMER 2
+        //chuj!
     }
 }
