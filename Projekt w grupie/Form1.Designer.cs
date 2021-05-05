a502bff5cebb66106e47d32c8b98bb5183278b4a
Damian Sygitowicz <UL0249095@edu.uni.lodz.pl>
2021-05-05 18:46:26 +0200

    dodałem button

--- Projekt w grupie/Form1.Designer.cs
+++ Projekt w grupie/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.autorzy = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -69,12 +70,24 @@
             this.autorzy.UseVisualStyleBackColor = true;
             this.autorzy.Click += new System.EventHandler(this.autorzy_Click);
             // 
+            // button3
+            // 
+            this.button3.Image = global::Projekt_w_grupie.Properties.Resources.odpowiedz;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.Location = new System.Drawing.Point(12, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(688, 94);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "NOWY BUTTON";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1527, 608);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.autorzy);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -89,6 +102,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button autorzy;
+        private System.Windows.Forms.Button button3;
     }
 }
 
