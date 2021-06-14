ec11bf6e01ebe3e8639dc02e4d618b15c94c90ef
BartoszCzerwinski123 <UL0238469@edu.uni.lodz.pl>
2021-06-14 13:42:23 +0200

    Dodawanie do tablicy wyników

--- Projekt w grupie/Form2.cs
+++ Projekt w grupie/Form2.cs
@@ -12,20 +12,39 @@ namespace Projekt_w_grupie
 {
     public partial class Form2 : Form
     {
+        DatabasewynikiDataContext DatabaseDC = new DatabasewynikiDataContext();
         public Form2(double suma_pkt)
         {
             InitializeComponent();
             button2.Text = suma_pkt.ToString();
             button3.Text = enterName.nickName;
+            
         }
+
+       
 
         private void nextQuestion_btn_Click_1(object sender, EventArgs e)
         {
+            wynik newwynik = new wynik();
+            newwynik.Nazwa = button3.Text;
+            newwynik.Wynik1 = button2.Text;
+
+            DatabaseDC.wyniks.InsertOnSubmit(newwynik);
+            DatabaseDC.SubmitChanges();
+
             Application.Exit();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            wynik newwynik = new wynik();
+            newwynik.Nazwa = button3.Text;
+            newwynik.Wynik1 = button2.Text;
+
+            DatabaseDC.wyniks.InsertOnSubmit(newwynik);
+            DatabaseDC.SubmitChanges();
+
             this.Close();
             Form1 gra = new Form1();
             gra.Show();
