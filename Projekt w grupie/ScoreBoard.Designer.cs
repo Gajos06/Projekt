112bde1d08fb972d075a109ba174a7d084a7a8af
Gajos06 <80858716+Gajos06@users.noreply.github.com>
2021-06-17 20:11:32 +0200

    juhuh

--- Projekt w grupie/ScoreBoard.Designer.cs
+++ Projekt w grupie/ScoreBoard.Designer.cs
@@ -38,11 +38,18 @@ namespace Projekt_w_grupie
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.guzik1 = new System.Windows.Forms.Button();
+            this.guzik1o = new System.Windows.Forms.Button();
+            this.guzik2o = new System.Windows.Forms.Button();
             this.guzik2 = new System.Windows.Forms.Button();
+            this.guzik3o = new System.Windows.Forms.Button();
             this.guzik3 = new System.Windows.Forms.Button();
+            this.guzik4o = new System.Windows.Forms.Button();
             this.guzik4 = new System.Windows.Forms.Button();
+            this.guzik5o = new System.Windows.Forms.Button();
             this.guzik5 = new System.Windows.Forms.Button();
+            this.guzik6o = new System.Windows.Forms.Button();
             this.guzik6 = new System.Windows.Forms.Button();
+            this.guzik7o = new System.Windows.Forms.Button();
             this.guzik7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -159,92 +166,169 @@ namespace Projekt_w_grupie
             // 
             // guzik1
             // 
-            this.guzik1.Enabled = false;
             this.guzik1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik1.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik1.Location = new System.Drawing.Point(76, 56);
             this.guzik1.Name = "guzik1";
-            this.guzik1.Size = new System.Drawing.Size(407, 40);
+            this.guzik1.Size = new System.Drawing.Size(190, 40);
             this.guzik1.TabIndex = 9;
             this.guzik1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik1.UseVisualStyleBackColor = true;
             // 
+            // guzik1o
+            // 
+            this.guzik1o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik1o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik1o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik1o.Location = new System.Drawing.Point(261, 56);
+            this.guzik1o.Name = "guzik1o";
+            this.guzik1o.Size = new System.Drawing.Size(222, 40);
+            this.guzik1o.TabIndex = 16;
+            this.guzik1o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik1o.UseVisualStyleBackColor = true;
+            // 
+            // guzik2o
+            // 
+            this.guzik2o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik2o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik2o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik2o.Location = new System.Drawing.Point(261, 102);
+            this.guzik2o.Name = "guzik2o";
+            this.guzik2o.Size = new System.Drawing.Size(222, 40);
+            this.guzik2o.TabIndex = 18;
+            this.guzik2o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik2o.UseVisualStyleBackColor = true;
+            // 
             // guzik2
             // 
-            this.guzik2.Enabled = false;
             this.guzik2.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik2.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik2.Location = new System.Drawing.Point(76, 102);
             this.guzik2.Name = "guzik2";
-            this.guzik2.Size = new System.Drawing.Size(407, 40);
-            this.guzik2.TabIndex = 10;
+            this.guzik2.Size = new System.Drawing.Size(190, 40);
+            this.guzik2.TabIndex = 17;
             this.guzik2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik2.UseVisualStyleBackColor = true;
             // 
+            // guzik3o
+            // 
+            this.guzik3o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik3o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik3o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik3o.Location = new System.Drawing.Point(261, 148);
+            this.guzik3o.Name = "guzik3o";
+            this.guzik3o.Size = new System.Drawing.Size(222, 40);
+            this.guzik3o.TabIndex = 20;
+            this.guzik3o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik3o.UseVisualStyleBackColor = true;
+            // 
             // guzik3
             // 
-            this.guzik3.Enabled = false;
             this.guzik3.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik3.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik3.Location = new System.Drawing.Point(76, 148);
             this.guzik3.Name = "guzik3";
-            this.guzik3.Size = new System.Drawing.Size(407, 40);
-            this.guzik3.TabIndex = 11;
+            this.guzik3.Size = new System.Drawing.Size(190, 40);
+            this.guzik3.TabIndex = 19;
             this.guzik3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik3.UseVisualStyleBackColor = true;
             // 
+            // guzik4o
+            // 
+            this.guzik4o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik4o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik4o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik4o.Location = new System.Drawing.Point(261, 194);
+            this.guzik4o.Name = "guzik4o";
+            this.guzik4o.Size = new System.Drawing.Size(222, 40);
+            this.guzik4o.TabIndex = 22;
+            this.guzik4o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik4o.UseVisualStyleBackColor = true;
+            // 
             // guzik4
             // 
-            this.guzik4.Enabled = false;
             this.guzik4.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik4.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik4.Location = new System.Drawing.Point(76, 194);
             this.guzik4.Name = "guzik4";
-            this.guzik4.Size = new System.Drawing.Size(407, 40);
-            this.guzik4.TabIndex = 12;
+            this.guzik4.Size = new System.Drawing.Size(190, 40);
+            this.guzik4.TabIndex = 21;
             this.guzik4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik4.UseVisualStyleBackColor = true;
             // 
+            // guzik5o
+            // 
+            this.guzik5o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik5o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik5o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik5o.Location = new System.Drawing.Point(261, 240);
+            this.guzik5o.Name = "guzik5o";
+            this.guzik5o.Size = new System.Drawing.Size(222, 40);
+            this.guzik5o.TabIndex = 24;
+            this.guzik5o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik5o.UseVisualStyleBackColor = true;
+            // 
             // guzik5
             // 
-            this.guzik5.Enabled = false;
             this.guzik5.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik5.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik5.Location = new System.Drawing.Point(76, 240);
             this.guzik5.Name = "guzik5";
-            this.guzik5.Size = new System.Drawing.Size(407, 40);
-            this.guzik5.TabIndex = 13;
+            this.guzik5.Size = new System.Drawing.Size(190, 40);
+            this.guzik5.TabIndex = 23;
             this.guzik5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik5.UseVisualStyleBackColor = true;
             // 
+            // guzik6o
+            // 
+            this.guzik6o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik6o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik6o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik6o.Location = new System.Drawing.Point(261, 286);
+            this.guzik6o.Name = "guzik6o";
+            this.guzik6o.Size = new System.Drawing.Size(222, 40);
+            this.guzik6o.TabIndex = 26;
+            this.guzik6o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik6o.UseVisualStyleBackColor = true;
+            // 
             // guzik6
             // 
-            this.guzik6.Enabled = false;
             this.guzik6.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik6.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik6.Location = new System.Drawing.Point(76, 286);
             this.guzik6.Name = "guzik6";
-            this.guzik6.Size = new System.Drawing.Size(407, 40);
-            this.guzik6.TabIndex = 14;
+            this.guzik6.Size = new System.Drawing.Size(190, 40);
+            this.guzik6.TabIndex = 25;
             this.guzik6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik6.UseVisualStyleBackColor = true;
             // 
+            // guzik7o
+            // 
+            this.guzik7o.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guzik7o.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guzik7o.Image = global::Projekt_w_grupie.Properties.Resources.general;
+            this.guzik7o.Location = new System.Drawing.Point(261, 332);
+            this.guzik7o.Name = "guzik7o";
+            this.guzik7o.Size = new System.Drawing.Size(222, 40);
+            this.guzik7o.TabIndex = 28;
+            this.guzik7o.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guzik7o.UseVisualStyleBackColor = true;
+            // 
             // guzik7
             // 
-            this.guzik7.Enabled = false;
             this.guzik7.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guzik7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guzik7.Image = global::Projekt_w_grupie.Properties.Resources.general;
             this.guzik7.Location = new System.Drawing.Point(76, 332);
             this.guzik7.Name = "guzik7";
-            this.guzik7.Size = new System.Drawing.Size(407, 40);
-            this.guzik7.TabIndex = 15;
+            this.guzik7.Size = new System.Drawing.Size(190, 40);
+            this.guzik7.TabIndex = 27;
             this.guzik7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.guzik7.UseVisualStyleBackColor = true;
             // 
@@ -253,13 +337,20 @@ namespace Projekt_w_grupie
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Projekt_w_grupie.Properties.Resources._135153_blue_solid_backgrounds_screensaver_2560x1600_h;
-            this.ClientSize = new System.Drawing.Size(493, 406);
+            this.ClientSize = new System.Drawing.Size(501, 402);
+            this.Controls.Add(this.guzik7o);
             this.Controls.Add(this.guzik7);
+            this.Controls.Add(this.guzik6o);
             this.Controls.Add(this.guzik6);
+            this.Controls.Add(this.guzik5o);
             this.Controls.Add(this.guzik5);
+            this.Controls.Add(this.guzik4o);
             this.Controls.Add(this.guzik4);
+            this.Controls.Add(this.guzik3o);
             this.Controls.Add(this.guzik3);
+            this.Controls.Add(this.guzik2o);
             this.Controls.Add(this.guzik2);
+            this.Controls.Add(this.guzik1o);
             this.Controls.Add(this.guzik1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -288,11 +379,18 @@ namespace Projekt_w_grupie
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button guzik1;
+        private System.Windows.Forms.Button guzik1o;
+        private System.Windows.Forms.Button guzik2o;
         private System.Windows.Forms.Button guzik2;
+        private System.Windows.Forms.Button guzik3o;
         private System.Windows.Forms.Button guzik3;
+        private System.Windows.Forms.Button guzik4o;
         private System.Windows.Forms.Button guzik4;
+        private System.Windows.Forms.Button guzik5o;
         private System.Windows.Forms.Button guzik5;
+        private System.Windows.Forms.Button guzik6o;
         private System.Windows.Forms.Button guzik6;
+        private System.Windows.Forms.Button guzik7o;
         private System.Windows.Forms.Button guzik7;
     }
 }
--- Projekt w grupie/ScoreBoard.cs
+++ Projekt w grupie/ScoreBoard.cs
@@ -27,10 +27,45 @@ namespace Projekt_w_grupie
 
         private void Loadwyniki()
         {
-        
 
 
 
+             var lista = DatabaseDC.wyniki.OrderByDescending(w => w.Punktacja).ToList();
+            if (lista[0] != null)
+            {
+                guzik1.Text = lista[0].Nazwa_użytkownika;
+                guzik1o.Text = lista[0].Punktacja;
+            }
+            if (lista[1] != null)
+            {
+                guzik2.Text = lista[1].Nazwa_użytkownika;
+                guzik2o.Text = lista[1].Punktacja;
+            }
+            if (lista[2] != null)
+            {
+                guzik3.Text = lista[2].Nazwa_użytkownika;
+                guzik3o.Text = lista[2].Punktacja;
+            }
+            if (lista[3] != null)
+            {
+                guzik4.Text = lista[3].Nazwa_użytkownika;
+                guzik4o.Text = lista[3].Punktacja;
+            }
+            if (lista[4] != null)
+            {
+                guzik5.Text = lista[4].Nazwa_użytkownika;
+                guzik5o.Text = lista[4].Punktacja;
+            }
+            if(lista[5] != null)
+            {
+                guzik6.Text = lista[5].Nazwa_użytkownika;
+                guzik6o.Text = lista[5].Punktacja;
+            }
+            if(lista[6] != null)
+            {
+                guzik7.Text = lista[6].Nazwa_użytkownika;
+                guzik7o.Text = lista[6].Punktacja;
+            }
         }
         
         
